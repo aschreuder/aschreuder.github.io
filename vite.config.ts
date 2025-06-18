a8eaa712989462262b4ea6ebdd0e39564a929753
Arnaud Schreuder <arnaudschreuder@gmail.com>
2025-06-18 11:52:34 +0200

    updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/',  // 👈 Add this line explicitly
   server: {
     host: "::",
     port: 8080,
